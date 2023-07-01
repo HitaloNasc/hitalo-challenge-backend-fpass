1e24057eb057145d4ca9765ba6a082277f5c9d4e
Hítalo Nascimento <hitalobruno2011@gmail.com>
2023-07-01 19:04:30 -0300

    refactor: remove unnecessary interfaces

--- src/characters/interfaces/character-list.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from '../entities/character.entity';
-
-export interface CharacterList {
-    offset: number;
-    limit: number;
-    total: number;
-    count: number;
-    results: Character[];
-}
--- src/characters/interfaces/index.ts
+++ src/characters/interfaces/index.ts
@@ -1,3 +1,1 @@
-export * from './character-fetch-response.interface';
-export * from './character-list.interface';
 export * from './character.interface';
