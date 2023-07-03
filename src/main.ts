80d900134440176f52a5f81ba2480cb54c738abf
Hítalo Nascimento <hitalobruno2011@gmail.com>
2023-07-03 09:57:11 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     app.enableCors();
     await app.listen(3000);
 }
